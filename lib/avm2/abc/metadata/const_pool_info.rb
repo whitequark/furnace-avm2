aa29737fdf8c4f77f07150feaef63b3cff17e0a2
Peter Zotov <whitequark@whitequark.org>
2012-01-08 18:38:41 +0400

    Optimize cpool definition.

--- lib/avm2/abc/metadata/const_pool_info.rb
+++ lib/avm2/abc/metadata/const_pool_info.rb
@@ -1,24 +1,18 @@
 module AVM2::ABC
   class ConstPoolInfo < Record
-    vuint30 :int_count,                            :value => lambda { ints.count + 1 }
-    array   :ints, :type => :vint32,               :initial_length => lambda { int_count - 1 }
+    def self.cpool_array_of(name, type)
+      field_size, field_array = :"#{name}_count", :"#{name}s"
 
-    vuint30 :uint_count,                           :value => lambda { uints.count + 1 }
-    array   :uints, :type => :vuint32,             :initial_length => lambda { uint_count - 1 }
+      vuint30 field_size,                 :value => lambda { send(field_array).count + 1 }
+      array   field_array, :type => type, :initial_length => lambda { send(field_size) - 1 }
+    end
 
-    vuint30 :double_count,                         :value => lambda { doubles.count + 1 }
-    array   :doubles, :type => :double,            :initial_length => lambda { double_count - 1 }
-
-    vuint30 :string_count,                         :value => lambda { strings.count + 1 }
-    array   :strings, :type => :string_info,       :initial_length => lambda { string_count - 1 }
-
-    vuint30 :namespace_count,                      :value => lambda { namespaces.count + 1 }
-    array   :namespaces, :type => :namespace_info, :initial_length => lambda { namespace_count - 1 }
-
-    vuint30 :ns_set_count,                         :value => lambda { ns_sets.count + 1 }
-    array   :ns_sets, :type => :ns_set_info,       :initial_length => lambda { ns_set_count - 1 }
-
-    vuint30 :multiname_count,                      :value => lambda { multinames.count + 1 }
-    array   :multinames, :type => :multiname_info, :initial_length => lambda { multiname_count - 1 }
+    cpool_array_of :int,       :vint32
+    cpool_array_of :uint,      :vuint32
+    cpool_array_of :double,    :double
+    cpool_array_of :string,    :string_info
+    cpool_array_of :namespace, :namespace_info
+    cpool_array_of :ns_set,    :ns_set_info
+    cpool_array_of :multiname, :multiname_info
   end
 end
