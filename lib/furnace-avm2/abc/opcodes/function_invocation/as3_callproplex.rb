79ce26139553f9cc259a9b4d81ab83d5b6b70c85
Peter Zotov <whitequark@whitequark.org>
2012-02-23 16:17:00 +0400

    Added callproplex opcode.

---
+++ lib/furnace-avm2/abc/opcodes/function_invocation/as3_callproplex.rb
@@ -0,0 +1,21 @@
+module Furnace::AVM2::ABC
+  class AS3CallPropertyLex < Opcode
+    include ContextualOpcode
+
+    instruction 0x4c
+
+    implicit_operand false
+
+    body do
+      const_ref :property, :multiname
+      vuint30   :arg_count
+    end
+
+    consume { body.arg_count }
+    produce 1
+
+    def disassemble_parameters
+      "#{body.property.to_s}(#{body.arg_count})"
+    end
+  end
+end
