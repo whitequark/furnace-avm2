211bd289e078402b9eac75c7183732987ed614c8
Peter Zotov <whitequark@whitequark.org>
2012-04-12 23:49:24 +0400

    Fix generic names.

--- lib/furnace-avm2/source/declaration_tokens/multiname_token.rb
+++ lib/furnace-avm2/source/declaration_tokens/multiname_token.rb
@@ -6,33 +6,47 @@ module Furnace::AVM2::Tokens
     end
 
     def to_text
-      if @multiname
+      name_to_text(@multiname)
+    end
+
+    protected
+
+    def name_to_text(multiname)
+      if multiname
         if @options[:debug_names]
-          debug = "/* #{@multiname.to_astlet.to_sexp} */ "
+          debug = "/* #{multiname.to_astlet.to_sexp} */ "
         end
 
-        qualified_name = ->(ns) {
-          if @options[:ns].include?(ns) || ["*", ""].include?(ns.name.to_s) || @options[:omit_ns]
-            "#{debug}#{@multiname.name}"
+        qualified_name = ->(ns, name) {
+          if @options[:ns].include?(ns) ||
+                ["*", ""].include?(ns.name) ||
+                @options[:omit_ns]
+            "#{debug}#{name}"
           else
-            "#{debug}#{ns.name}.#{@multiname.name}"
+            "#{debug}#{ns.name}.#{name}"
           end
         }
 
-        case @multiname.kind
+        case multiname.kind
         when :QName, :QNameA
-          qualified_name.(@multiname.ns)
+          qualified_name.(multiname.ns, multiname.name)
         when :GenericName
-          "#{debug}#{@multiname.name.name}.<#{@multiname.parameters.map(&:name).join}>"
+          parameters = multiname.parameters.map do |param|
+            subname = name_to_text(param)
+            # Fuck you.
+            subname = "#{subname} " if subname.end_with? ">"
+            subname
+          end
+          "#{debug}#{multiname.name.name}.<#{parameters.join(", ")}>"
         when :Multiname
-          if @multiname.ns_set.ns.count == 1
-            qualified_name.(@multiname.ns_set.ns[0])
+          if multiname.ns_set.ns.count == 1
+            qualified_name.(multiname.ns_set.ns[0], multiname.name)
           else
-            "/* #{@multiname.to_astlet.to_sexp} */ " +
-                qualified_name.(@multiname.ns_set.ns[0])
+            "/* #{multiname.to_astlet.to_sexp} */ " +
+                qualified_name.(multiname.ns_set.ns[0], multiname.name)
           end
         else
-          "#{debug}%%#{@multiname.kind}"
+          "#{debug}%%#{multiname.kind}"
         end
       else
         "%%nil"
