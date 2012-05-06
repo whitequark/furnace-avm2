whitequark/furnace-avm2

commit 2bd7bffb94168154b4ef73d1ac0e774c2ba341c9

Fix CFG block splitting on labels.

lib/furnace-avm2/transform/cfg_build.rb
@@ -32,6 +32,10 @@ module Furnace::AVM2
         @pending_queue = []
 
         ast.children.each_with_index do |node, index|
+          if node.type == :label
+            cutoff(nil, [ node.metadata[:label] ])
+          end
+
           unless @pending_label
             @pending_label = node.metadata[:label]
 
@@ -48,7 +52,6 @@ module Furnace::AVM2
 
           case node.type
           when :label
-            @jumps.add node.children.first
             node.update :nop
 
           when :return_value, :return_void

commit cf5d43e33c97007d451bba2043db76cfd3688227

Fix a bug introduced in 2bd7bff.

lib/furnace-avm2/transform/cfg_build.rb
@@ -32,10 +32,6 @@ module Furnace::AVM2
         @pending_queue = []
 
         ast.children.each_with_index do |node, index|
-          if node.type == :label
-            cutoff(nil, [ node.metadata[:label] ])
-          end
-
           unless @pending_label
             @pending_label = node.metadata[:label]
 
@@ -73,7 +69,7 @@ module Furnace::AVM2
           else
             *, next_exception_block = exception_block_for(next_label)
 
-            if @jumps.include?(next_label)
+            if @jumps.include?(next_label) || next_node.type == :label
               cutoff(nil, [next_label])
             elsif @pending_exc_block != next_exception_block
               cutoff(nil, [next_label])