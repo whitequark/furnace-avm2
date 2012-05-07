whitequark/furnace-avm2

commit e40eea68ef232961d4a82995376ba3140957e441

Flush stack upon encountering function return.

lib/furnace-avm2/transform/ast_build.rb
@@ -195,6 +195,14 @@ module Furnace::AVM2
             node.children.concat opcode.parameters
             node.children.concat parameters
 
+            # Spec does not require stack to be empty upon encountering return.
+            # If there's something left, it should have been here.
+            if opcode.is_a? ABC::FunctionReturnOpcode
+              while @stack.any?
+                emit(*consume(1))
+              end
+            end
+
             # All opcodes which produce 2 results--that is, dup and swap--
             # are already handled at the top.
             if opcode.produces == 0

lib/furnace-avm2/abc.rb
@@ -16,6 +16,7 @@ require_relative "abc/opcodes/load_store_opcode"
 require_relative "abc/opcodes/push_literal_opcode"
 require_relative "abc/opcodes/control_transfer_opcode"
 require_relative "abc/opcodes/function_invocation_opcode"
+require_relative "abc/opcodes/function_return_opcode"
 require_relative "abc/opcodes/property_opcode"
 
 Dir[File.join(File.dirname(__FILE__), "abc", "opcodes", "*", "*.rb")].each do |file|

lib/furnace-avm2/abc/opcodes/function_return/as3_returnvalue.rb
@@ -1,5 +1,5 @@
 module Furnace::AVM2::ABC
-  class AS3ReturnValue < Opcode
+  class AS3ReturnValue < FunctionReturnOpcode
     instruction 0x48
 
     consume 1

lib/furnace-avm2/abc/opcodes/function_return/as3_returnvoid.rb
@@ -1,5 +1,5 @@
 module Furnace::AVM2::ABC
-  class AS3ReturnVoid < Opcode
+  class AS3ReturnVoid < FunctionReturnOpcode
     instruction 0x47
 
     consume 0

lib/furnace-avm2/abc/opcodes/function_return_opcode.rb
@@ -1,0 +1,4 @@
+module Furnace::AVM2::ABC
+  class FunctionReturnOpcode < Opcode
+  end
+ end

commit ab6ffaf3d9918cf0b0dc0eb898ae32b30559327c

Fix a bug with corner case on ternary operator introduced in e40eea68.

lib/furnace-avm2/transform/ast_build.rb
@@ -195,18 +195,19 @@ module Furnace::AVM2
             node.children.concat opcode.parameters
             node.children.concat parameters
 
-            # Spec does not require stack to be empty upon encountering return.
-            # If there's something left, it should have been here.
-            if opcode.is_a? ABC::FunctionReturnOpcode
-              while @stack.any?
-                emit(*consume(1))
-              end
-            end
-
             # All opcodes which produce 2 results--that is, dup and swap--
             # are already handled at the top.
             if opcode.produces == 0
               expand_conditionals()
+
+              # Spec does not require stack to be empty upon encountering return.
+              # If there's something left, it should have been here.
+              if opcode.is_a? ABC::FunctionReturnOpcode
+                while @stack.any?
+                  emit(*consume(1))
+                end
+              end
+
               emit(node)
             elsif opcode.produces == 1
               produce(node)